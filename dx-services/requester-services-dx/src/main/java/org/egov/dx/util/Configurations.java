ac9fa707b2987423a3ff5ad8e936cf439f283020
viral-v <viral.variya@pwc.com>
2026-07-01 14:43:16 +0530

    emudra changes
    emudra changes

--- dx-services/requester-services-dx/src/main/java/org/egov/dx/util/Configurations.java
+++ dx-services/requester-services-dx/src/main/java/org/egov/dx/util/Configurations.java
@@ -1,7 +1,16 @@
 package org.egov.dx.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import lombok.Getter;
 
@@ -147,7 +156,8 @@ public class Configurations {
 	
 	@Value("${esign.signature.content.size}")
 	private int SignatureContentSize;
+
+	@Value("${esign.gateway.url}")
+	private int gatewayUrl;
 	
-
-
 }
